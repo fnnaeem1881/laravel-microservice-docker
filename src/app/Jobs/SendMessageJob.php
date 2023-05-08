5b97ea5e471001956764ce83ac755329fc14c78d
fnnaeem1881 <mehedinaeem66@gmail.com>
2023-05-08 11:12:42 +0600

    message send

--- src/app/Jobs/SendMessageJob.php
+++ src/app/Jobs/SendMessageJob.php
@@ -1,21 +1,19 @@
 <?php
 
 namespace App\Jobs;
+
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
 
 class SendMessageJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    /**
-     * The message to send.
-     *
-     * @var string
-     */
     protected $message;
 
     /**
@@ -36,12 +34,48 @@ class SendMessageJob implements ShouldQueue
      */
     public function handle()
     {
-        $connection = app('rabbitmq')->connection();
+
+
+        // $host = 'goose-01.rmq2.cloudamqp.com'; // replace with your container name
+        // $port = 5672;
+        // $user = 'xrljngis';
+        // $password = '4tdn42J7Dpq4oXL9QH-IIrU4WkyPfSu';
+        // $vhost = 'xrljngis';
+
+        $host = 'rabbitmq'; // replace with your container name
+        $port = 5672;
+        $user = 'guest';
+        $password = 'guest';
+        $vhost = '/';
+
+        $exchange = 'my_exchange'; // replace with your own exchange name
+        $queue = 'default'; // replace with your own queue name
+        $messageBody = $this->message;
+
+        // create a connection to RabbitMQ
+        $connection = new AMQPStreamConnection($host, $port, $user, $password, $vhost);
+
+        // create a channel
         $channel = $connection->channel();
-        $channel->queue_declare('test_queue', false, true, false, false);
-        $msg = new \PhpAmqpLib\Message\AMQPMessage($this->message);
-        $channel->basic_publish($msg, '', 'test_queue');
+
+        // declare an exchange
+        $channel->exchange_declare($exchange, 'direct', false, true, false);
+
+        // declare a queue
+        $channel->queue_declare($queue, false, true, false, false);
+
+        // bind the queue to the exchange
+        $channel->queue_bind($queue, $exchange);
+
+        // create a message
+        $message = new AMQPMessage($messageBody);
+
+        // publish the message to the exchange
+        $channel->basic_publish($message, $exchange);
+
+        // close the channel and the connection
         $channel->close();
         $connection->close();
+
     }
 }
